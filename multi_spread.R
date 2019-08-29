d8d36ca828ba9210e0c4a9e14bda749558585c32
DomMacWork <dgrisafe@usc.edu>
2019-08-28 22:20:44 -0700

    Multispread and related functions

---
+++ multi_spread.R
@@ -0,0 +1,11 @@
+multi_spread <-
+function(df, key, value) {
+    # quote key
+    keyq <- rlang::enquo(key)
+    # break value vector into quotes
+    valueq <- rlang::enquo(value)
+    s <- rlang::quos(!!valueq)
+    df %>% gather(variable, value, !!!s) %>%
+        unite(temp, !!keyq, variable) %>%
+        spread(temp, value)
+}
